4be8f54de6ee79f532b5ab2bd189b909c7ecf62e
Gabriel Dunne <gdunne@quilime.com>
2015-07-06 18:54:58 -0700

    Some code cleanup.

--- src/HttpRequest.cpp
+++ src/HttpRequest.cpp
@@ -6,24 +6,18 @@ using namespace std;
 using namespace ci;
 using namespace ci::app;
 
-ScopedAsyncHttpRequest AsyncHttpRequest::create(asio::io_service &service, std::string hostname)
-{
+ScopedAsyncHttpRequest AsyncHttpRequest::create(asio::io_service &service, std::string hostname) {
 	return std::make_shared<AsyncHttpRequest>(service, hostname);
 }
 
 AsyncHttpRequest::AsyncHttpRequest(asio::io_service &service, std::string hostname)
-: _resolver(service), _socket(service), _hostname(hostname)
-{
-
+: _resolver(service), _socket(service), _hostname(hostname) {
 }
 
-AsyncHttpRequest::~AsyncHttpRequest()
-{
-    
+AsyncHttpRequest::~AsyncHttpRequest() {
 }
 
-void AsyncHttpRequest::BeginGet(std::string path, std::function<void(std::string)>&& Callback)
-{
+void AsyncHttpRequest::BeginGet(std::string path, std::function<void(std::string)>&& Callback) {
 
 	tcp::resolver::query	query(_hostname, "http");
 
@@ -35,36 +29,35 @@ void AsyncHttpRequest::BeginGet(std::string path, std::function<void(std::string
 
 	_callback = Callback;
 	_resolver.async_resolve(query,
-		std::bind(&AsyncHttpRequest::handle_resolve, shared_from_this(), std::placeholders::_1, std::placeholders::_2));
-
-
-
-
+		std::bind(&AsyncHttpRequest::handle_resolve,
+              shared_from_this(), std::placeholders::_1, std::placeholders::_2));
 }
 
-void AsyncHttpRequest::BeginPost(std::string path, JsonTree reqData, std::function<void(std::string)>&& Callback)
-{
+void AsyncHttpRequest::BeginPost(std::string path, JsonTree reqData,
+                                 std::function<void(std::string)>&& Callback) {
+
 	tcp::resolver::query query(_hostname, "http");
 
-    string paramstr = reqData.serialize();
+  string paramstr = reqData.serialize();
 
 	std::ostream request_stream(&_request);
 	request_stream << "POST " << path << " HTTP/1.1\r\n";
 	request_stream << "Host: " << _hostname << "\r\n";
 	request_stream << "Accept: */*\r\n";
 	request_stream << "Connection: close\r\n";
+  request_stream << "Content-Type: application/json; charset=utf-8\r\n";
 	request_stream << "Content-Length: " << paramstr.size() << "\r\n\r\n";
 	request_stream << paramstr;
-    
-    //console() << request_stream.rdbuf();
 
 	_callback = Callback;
 	_resolver.async_resolve(query,
-		std::bind(&AsyncHttpRequest::handle_resolve, shared_from_this(), std::placeholders::_1, std::placeholders::_2));
+		std::bind(&AsyncHttpRequest::handle_resolve, shared_from_this(),
+              std::placeholders::_1, std::placeholders::_2));
 }
 
-void AsyncHttpRequest::BeginPut(std::string path, JsonTree reqData, std::function<void(std::string)>&& Callback)
-{
+void AsyncHttpRequest::BeginPut(std::string path, JsonTree reqData,
+                                std::function<void(std::string)>&& Callback) {
+
 	std::string paramstr = reqData.serialize();
 	tcp::resolver::query	query(_hostname, "http");
 
@@ -78,81 +71,72 @@ void AsyncHttpRequest::BeginPut(std::string path, JsonTree reqData, std::functio
 
 	_callback = Callback;
 	_resolver.async_resolve(query,
-		std::bind(&AsyncHttpRequest::handle_resolve, shared_from_this(), std::placeholders::_1, std::placeholders::_2));
+                          std::bind(&AsyncHttpRequest::handle_resolve, shared_from_this(),
+                          std::placeholders::_1, std::placeholders::_2));
 }
 
-void AsyncHttpRequest::BeginDelete(std::string path, JsonTree reqData, std::function<void(std::string)>&& Callback)
-{
+void AsyncHttpRequest::BeginDelete(std::string path, JsonTree reqData,
+                                   std::function<void(std::string)>&& Callback) {
 	_callback = Callback;
     
-    std::string paramstr = reqData.serialize();
-    tcp::resolver::query	query(_hostname, "http");
-    
-    std::ostream request_stream(&_request);
-    request_stream << "DELETE " << path << " HTTP/1.1\r\n";
-    request_stream << "Host: " << _hostname << "\r\n";
-    request_stream << "Accept: */*\r\n";
-    request_stream << "Connection: close\r\n";
-    request_stream << "Content-Length: " << paramstr.size() << "\r\n\r\n";
-    request_stream << paramstr;
-    
-    _resolver.async_resolve(query,
-            std::bind(&AsyncHttpRequest::handle_resolve, shared_from_this(), std::placeholders::_1, std::placeholders::_2));
+  std::string paramstr = reqData.serialize();
+  tcp::resolver::query	query(_hostname, "http");
+
+  std::ostream request_stream(&_request);
+  request_stream << "DELETE " << path << " HTTP/1.1\r\n";
+  request_stream << "Host: " << _hostname << "\r\n";
+  request_stream << "Accept: */*\r\n";
+  request_stream << "Connection: close\r\n";
+  request_stream << "Content-Length: " << paramstr.size() << "\r\n\r\n";
+  request_stream << paramstr;
+
+  _resolver.async_resolve(query,
+                          std::bind(&AsyncHttpRequest::handle_resolve,
+                          shared_from_this(), std::placeholders::_1, std::placeholders::_2));
 }
 
-void AsyncHttpRequest::handle_resolve(const asio::error_code& err, asio::ip::tcp::resolver::iterator endpt_itr)
-{
-	if (!err)
-	{
+void AsyncHttpRequest::handle_resolve(const asio::error_code& err,
+                                      asio::ip::tcp::resolver::iterator endpt_itr) {
+	if (!err) {
 		_endpoint = *endpt_itr;
 		try{
 			_socket.async_connect(_endpoint,
 				std::bind(&AsyncHttpRequest::handle_connect, shared_from_this(),
 				std::placeholders::_1, ++endpt_itr));
+		} catch (Exception e) {
 		}
-		catch (Exception e)
-		{
-            
-		}
-	}
-	else
-	{
+	} else {
 		handle_error(err);
 	}
 }
 
-void AsyncHttpRequest::handle_connect(const asio::error_code& err, asio::ip::tcp::resolver::iterator endpt_itr)
-{
+void AsyncHttpRequest::handle_connect(const asio::error_code& err,
+                                      asio::ip::tcp::resolver::iterator endpt_itr) {
 	if (!err) {
-		asio::async_write(_socket, _request, std::bind(&AsyncHttpRequest::handle_write, shared_from_this(), std::placeholders::_1));
+		asio::async_write(_socket, _request, std::bind(&AsyncHttpRequest::handle_write,
+                                                   shared_from_this(), std::placeholders::_1));
 	}
 	else {
 		handle_error(err);
 	}
 }
 
-void AsyncHttpRequest::handle_write(const asio::error_code& err)
-{
+void AsyncHttpRequest::handle_write(const asio::error_code& err) {
 	if (!err)
 	{
 		// Read the response status line. The response_ streambuf will
 		// automatically grow to accommodate the entire line. The growth may be
 		// limited by passing a maximum size to the streambuf constructor.
-
 		asio::async_read_until(_socket, _response, "\r\n",
 			std::bind(&AsyncHttpRequest::handle_read_status_line, shared_from_this(),
 			std::placeholders::_1, std::placeholders::_2));
-	}
-	else
-	{
+	} else {
 		handle_error(err);
 	}
 }
 
-void AsyncHttpRequest::handle_read_status_line(const asio::error_code& err, size_t bytesTransfered)
-{
-	if (!err)
-	{
+void AsyncHttpRequest::handle_read_status_line(const asio::error_code& err, size_t bytesTransfered) {
+	if (!err) {
 		// Check that response is OK.
 		std::istream response_stream(&_response);
 		std::string http_version;
@@ -174,23 +158,17 @@ void AsyncHttpRequest::handle_read_status_line(const asio::error_code& err, size
             _callback(ss.str());
 			return;
 		}
-
-
 		// Read the response headers, which are terminated by a blank line.
 		asio::async_read_until(_socket, _response, "\r\n\r\n",
-			std::bind(&AsyncHttpRequest::handle_read_headers, shared_from_this(),
-			std::placeholders::_1, std::placeholders::_2));
-	}
-	else
-	{
+                           std::bind(&AsyncHttpRequest::handle_read_headers, shared_from_this(),
+                                     std::placeholders::_1, std::placeholders::_2));
+	} else {
 		handle_error(err);
 	}
 }
 
-void AsyncHttpRequest::handle_read_headers(const asio::error_code& err, size_t bytesTransferred)
-{
-	if (!err)
-	{
+void AsyncHttpRequest::handle_read_headers(const asio::error_code& err, size_t bytesTransferred) {
+	if (!err) {
 		// Process the response headers.
 		std::istream response_stream(&_response);
 		std::string header;
@@ -200,48 +178,34 @@ void AsyncHttpRequest::handle_read_headers(const asio::error_code& err, size_t b
 			asio::transfer_at_least(1),
 			std::bind(&AsyncHttpRequest::handle_read_content, shared_from_this(),
 			std::placeholders::_1, std::placeholders::_2));
-	}
-	else
-	{
+	} else {
 		handle_error(err);
 	}
 }
 
-void AsyncHttpRequest::handle_read_content(const asio::error_code& err, size_t bytesTransferred)
-{
-	if (!err)
-	{
-
+void AsyncHttpRequest::handle_read_content(const asio::error_code& err, size_t bytesTransferred) {
+	if (!err) {
 		// Continue reading remaining data until EOF.
 		asio::async_read(_socket, _response,
-			asio::transfer_at_least(1),
-			std::bind(&AsyncHttpRequest::handle_read_content, shared_from_this(),
-			std::placeholders::_1, std::placeholders::_2));
-	}
-	else if (err != asio::error::eof)
-	{
-
+                     asio::transfer_at_least(1),
+                     std::bind(&AsyncHttpRequest::handle_read_content, shared_from_this(),
+                               std::placeholders::_1, std::placeholders::_2));
+	} else if (err != asio::error::eof) {
 		//Make sure socket closes
-        handle_error(err);
-	}
-	else
-	{
+    handle_error(err);
+	} else {
 		//Make sure socket closes / call handler
 		std::istream response_stream(&_response);
 		std::stringstream ss;
 		ss << response_stream.rdbuf();
-
-        
+    
 		_callback(ss.str());
-
 	}
 }
 
-void AsyncHttpRequest::handle_error(const asio::error_code& err)
-{
+void AsyncHttpRequest::handle_error(const asio::error_code& err) {
 	std::stringstream ss;
 	ss << "Client Error Caught By Handle_Resolve: " << err;
-	
 	_socket.close();
 	_callback(ss.str());
 }
--- src/HttpRequest.h
+++ src/HttpRequest.h
@@ -17,24 +17,23 @@ class AsyncHttpRequest : public std::enable_shared_from_this<AsyncHttpRequest>
 public:
 
 	static ScopedAsyncHttpRequest create(asio::io_service &service, std::string hostname);
-
-    void BeginGet(std::string path, std::function<void(std::string)>&& Callback = [](std::string s){});
-    void BeginPost(std::string path, ci::JsonTree reqData = ci::JsonTree(), std::function<void(std::string)>&& Callback = [](std::string){});
-	void BeginPut(std::string path, ci::JsonTree reqData = ci::JsonTree(), std::function<void(std::string)>&& Callback = [](std::string){});
-    void BeginDelete(std::string path, ci::JsonTree reqData = ci::JsonTree(), std::function<void(std::string)>&& Callback = [](std::string){});
-
-
-    
-    AsyncHttpRequest(asio::io_service &service, std::string hostname);
+  
+  void BeginGet(std::string path, std::function<void(std::string)>&& Callback = [](std::string s){});
+  void BeginPost(std::string path, ci::JsonTree reqData = ci::JsonTree(),
+                 std::function<void(std::string)>&& Callback = [](std::string){});
+	void BeginPut(std::string path, ci::JsonTree reqData = ci::JsonTree(),
+                std::function<void(std::string)>&& Callback = [](std::string){});
+  void BeginDelete(std::string path, ci::JsonTree reqData = ci::JsonTree(),
+                   std::function<void(std::string)>&& Callback = [](std::string){});
+  
+  AsyncHttpRequest(asio::io_service &service, std::string hostname);
 	~AsyncHttpRequest();
 
-
 private:
-	
+
 	void handle_resolve(const asio::error_code& err, asio::ip::tcp::resolver::iterator endpt_itr);
 	void handle_connect(const asio::error_code& err, asio::ip::tcp::resolver::iterator endpt_itr);
 	void handle_write(const asio::error_code& err);
-
 	void handle_read_status_line(const asio::error_code& err, size_t bytesTransferred);
 	void handle_read_headers(const asio::error_code& err, size_t bytesTransferred);
 	void handle_read_content(const asio::error_code& err, size_t bytesTransferred	);
@@ -43,13 +42,10 @@ private:
 
 	std::function <void(std::string)> _callback;
 
-    
 	asio::ip::tcp::resolver _resolver;
 	asio::ip::tcp::socket	_socket;
 	asio::ip::tcp::endpoint	_endpoint;
-	std::string				_hostname;
-	asio::streambuf			_request;
-	asio::streambuf			_response;
-
-
+	std::string _hostname;
+	asio::streambuf _request;
+	asio::streambuf _response;
 };
